49d2b5ebba9448599e4953a4ba07bc378e7abb0c
Gildo Duarte <gildo.duartec@gmail.com>
2023-12-28 23:36:10 -0300

    Added CORS, Throttlers and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-
 import { DomainModule } from './model/domain.module';
 import { ControllersModule } from './controllers/controllers.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DomainModule, ControllersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    DomainModule,
+    ControllersModule,
+    AuthModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          limit: 10,
+          ttl: 60,
+        },
+      ],
+    }),
+  ],
 })
 export class AppModule {}
